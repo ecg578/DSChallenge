5f6ccd7049325c226c38465c804564d81c1c6fab
ecg578 <ecg578@inlumine.ual.es>
2023-07-06 22:39:42 +0200

    commit

--- DataEngineerChallenge.py
+++ DataEngineerChallenge.py
@@ -2,6 +2,7 @@ def traverse_labyrinth(labyrinth, start, end):
     # Labyrinth dimensions
     rows = len(labyrinth)
     columns = len(labyrinth[0])
+    horizontal = True
 
     # List of directions to move in the labyrinth (up, down, left, right)
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -10,9 +11,22 @@ def traverse_labyrinth(labyrinth, start, end):
     def heuristic_distance(current_pos):
         return abs(current_pos[0] - end[0]) + abs(current_pos[1] - end[1])
 
+    def can_rotate(row, col,horizontal):
+        if (row-1 and col == '.') and (row+1 and col == '.') and (row and col-1 == '.') and (row and col+1 == '.') and (row-1 and col-1=='.') and (row+1 and col+1=='.') and (row-1 and col+1=='.') and (row+1 and col-1=='.'):
+            return not horizontal
+        else: 
+            return horizontal
+
+
     # Helper function to check if a position is within the labyrinth and is traversable
-    def is_valid(row, col):
-        return row >= 0 and row < rows and col >= 0 and col < columns and labyrinth[row][col] == '.'
+    def is_valid(row, col, horizontal):
+        if (row >= 0 and row < rows and col >= 0 and col < columns and labyrinth[row][col] == '.' and horizontal):
+            return (row-1 and col == '.') and (row+1 and col == '.')
+        elif (row >= 0 and row < rows and col >= 0 and col < columns and labyrinth[row][col] == '.' and not horizontal):
+            return (row and col-1 == '.') and (row and col+1 == '.')
+        else:
+            return False
+
 
     # Helper function to reconstruct the path from start to end
     def reconstruct_path(parents, current):
@@ -44,8 +58,9 @@ def traverse_labyrinth(labyrinth, start, end):
             new_col = current[1] + direction[1]
             new_pos = (new_row, new_col)
 
-            if is_valid(new_row, new_col):
+            if is_valid(new_row, new_col, horizontal):
                 g_score_new_pos = g_scores[current] + 1
+                #horizontal= can_rotate(new_row,new_col,horizontal)
                 if new_pos not in g_scores or g_score_new_pos < g_scores[new_pos]:
                     # Found a shorter path to this position
                     parents[new_pos] = current
@@ -66,18 +81,16 @@ labyrinth = [
     ['.', '#', '.', '.', '.', '.', '.', '#', '.']
 ]
 
-rod = (0,2)
 start = (0, 0)
-end = (3, 8)
+end = (4, 8)
 
 path, total_steps = traverse_labyrinth(labyrinth, start, end)
 
 if total_steps == -1:
-    print("No valid path found.")
+    print(-1)
 else:
-    print("Path found:")
     for row, col in path:
         labyrinth[row][col] = 'O'
     for row in labyrinth:
         print(' '.join(row))
-    print("Total steps:", total_steps)
+    print(total_steps)
